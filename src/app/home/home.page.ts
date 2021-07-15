0b9509b659d433e374f3eafa8962994c983928f5
Karan Ubhi <karan_u26@hotmail.com>
2021-07-15 19:06:34 -0400

    Added Button Functionality

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,7 +12,10 @@ import { SizesService } from '../size/sizes.service';
 export class HomePage {
   toppings: Toppings[];
   sizes: Sizes[];
+  sizeUpdate: string;
+  toppingsUpdate: string;
   itemSelected: any;
+  numberSelected: number;
   constructor(private toppingsService: ToppingsService, private sizesService: SizesService) {}
 
   ngOnInit(){
@@ -22,12 +25,51 @@ export class HomePage {
 
   ionViewWillEnter(){ }
 
-  onClick(size){
-    if(this.isSelected(size)){
-      this.itemSelected = null;
-  }else{
-      this.itemSelected = size;
+  sizeValue($event){
+    this.sizeUpdate = $event.target.value;
   }
-  return this.itemSelected === size;
-}
+
+
+  toppingValue($event){
+    this.toppingsUpdate = $event.target.value;
+  }
+
+  buttonOneClicked(){
+    this.numberSelected = 1;
+  }
+  buttonTwoClicked(){
+    this.numberSelected = 2;
+  }
+  buttonThreeClicked(){
+    this.numberSelected = 3;
+  }
+  buttonFourClicked(){
+    this.numberSelected = 4;
+  }
+  buttonFiveClicked(){
+    this.numberSelected = 5;
+  }
+  buttonSixClicked(){
+    this.numberSelected = 6;
+  }
+  buttonSevenClicked(){
+    this.numberSelected = 7;
+  }
+  buttonEightClicked(){
+    this.numberSelected = 8;
+  }
+  buttonNineClicked(){
+    this.numberSelected = 9;
+  }
+  buttonZeroClicked(){
+    this.numberSelected = 0;
+  }
+
+  resetButton(){
+    this.numberSelected = 0;
+  }
+
+  addButtonClicked(){
+
+  }
 }
